e11aeddef71202c2831b862556e8de8265dae489
simon bhattacharya <simonsaysooook@gmail.com>
2021-01-29 18:05:04 +0530

    added a new app(component) to the main site django project

--- mywebhomepage/urls.py
+++ mywebhomepage/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from polls.views import home
+from polls import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home')
 ]
